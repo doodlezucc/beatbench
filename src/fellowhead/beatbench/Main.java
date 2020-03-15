22d73b099cace95e3be8f05ab286fe86c3cf0a5a
FellowHead <thefellowhead@gmail.com>
2020-03-15 12:33:26 +0000

    off to a great start (vs code)

---
+++ src/fellowhead/beatbench/Main.java
@@ -0,0 +1,13 @@
+package fellowhead.beatbench;
+
+import fellowhead.beatbench.layout.MainFrame;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        demo();
+    }
+
+    public static void demo() {
+        new MainFrame();
+    }
+}
