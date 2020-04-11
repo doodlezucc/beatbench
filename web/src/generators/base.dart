6d4039984d547f7957f91bea6c18299aa34c48d8
FellowHead <thefellowhead@gmail.com>
2020-04-11 07:37:56 +0000

    introduced style scoping

--- web/src/generators/base.dart
+++ web/src/generators/base.dart
@@ -1,6 +1,9 @@
 import 'dart:html';
 import 'dart:web_audio';
 
+import 'package:csslib/parser.dart' as css;
+import 'package:csslib/visitor.dart';
+
 import '../notes.dart';
 import '../windows.dart';
 
@@ -10,13 +13,20 @@ abstract class GeneratorInterface<G extends Generator> extends Window {
 
   void _init(G gen) async {
     title = gen.name;
+    element.id = styleId;
 
     Document html = await _loadFile('$htmlPath', 'document');
     var validNodes = html.querySelector('generator').children.toList();
     validNodes.forEach((v) => element.append(v));
 
     var srcCss = await _loadFile('$cssPath', 'text');
-    print(srcCss);
+
+    var errors = <css.Message>[];
+    var parsedStyleSheet = css.parse(srcCss, errors: errors);
+    print('Parse error count: ${errors.length}');
+    errors.forEach((err) {
+      print('ERROR: ${err.level.index} - ${err.message}');
+    });
 
     // create a stylesheet element
     var styleElement = StyleElement();
@@ -24,11 +34,36 @@ abstract class GeneratorInterface<G extends Generator> extends Window {
     // use the styleSheet from that
     CssStyleSheet sheet = styleElement.sheet;
 
-    final rule = 'div { border: 1px solid red; }';
-    sheet.insertRule(rule, 0);
+    parsedStyleSheet.topLevels.forEach((top) {
+      var rule = '';
+      if (top is RuleSet) {
+        var printer = CssPrinter();
+        top.selectorGroup.visit(printer);
 
-    sheet.insertRule(srcCss);
-    print(sheet.cssRules);
+        if (printer.toString() == 'generator') {
+          rule = '.generator#$styleId';
+        } else {
+          // modify selectors
+          var selectors = top.selectorGroup.selectors;
+          for (var i = 0; i < selectors.length; i++) {
+            var printer = CssPrinter();
+            selectors[i].visit(printer);
+            if (i > 0) {
+              rule += ', ';
+            }
+            rule += '#$styleId ' + printer.toString();
+          }
+        }
+        // include rules
+        printer = CssPrinter();
+        top.declarationGroup.visit(printer);
+        var declaration = printer.toString();
+        rule += '{ $declaration }';
+      }
+      //print('inserting rule');
+      //print(rule);
+      sheet.insertRule(rule);
+    });
 
     domInit(gen);
   }
